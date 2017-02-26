7c3b5e6740e576fa396d9deaaff7b603119b4ac2
Junior Samaroo <jns0987654321@gmail.com>
2017-02-25 19:05:17 -0500

    Finished UI layout

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Junior Samaroo on 2017-02-25.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
